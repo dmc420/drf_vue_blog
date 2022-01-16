f13c1b734954aed57cfd018220635388bb05c870
dmc420 <dmc420@qq.com>
2022-01-16 14:36:52 +0800

    Article Detail的增删改查

--- article/serializers.py
+++ article/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
+from article.models import Article
 
 
-class ArticleListSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(allow_blank=True, max_length=100)
-    body = serializers.CharField(allow_blank=True)
-    created = serializers.DateTimeField()
-    updated = serializers.DateTimeField()
+class ArticleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = [
+            'id',
+            'title',
+            'body',
+            'created',
+            'updated'
+        ]
+
+
+class ArticleDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
--- article/views.py
+++ article/views.py
@@ -1,12 +1,53 @@
-from django.shortcuts import render
-
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from django.http import Http404
 from article.models import Article
-# 这个 ArticleListSerializer 暂时还没有
-from article.serializers import ArticleListSerializer
+from article.serializers import ArticleListSerializer, ArticleDetailSerializer
 
 
+@api_view(['GET', 'POST'])
 def article_list(request):
-    articles = Article.objects.all()
-    serializer = ArticleListSerializer(articles, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ArticleListSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetail(APIView):
+    """
+    文章详情视图
+    """
+
+    @staticmethod
+    def get_object(pk):
+        try:
+            return Article.objects.get(pk=pk)
+        except:
+            raise Http404
+
+    def get(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleDetailSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleDetailSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        article = self.get_object(pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
